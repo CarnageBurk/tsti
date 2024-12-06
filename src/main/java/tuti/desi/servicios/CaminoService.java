c0739b049c8182502d1999d6f069d454e51dd173
CarnageBurk <santi.burk@gmail.com>
2024-12-06 11:38:47 -0300

    ciudades agregadas

--- src/main/java/tuti/desi/servicios/CaminoService.java
+++
@@ -1,8 +0,0 @@
-package tuti.desi.servicios;
-
-import tuti.desi.entidades.Camino;
-import java.util.List;
-
-public interface CaminoService {
-    List<Camino> obtenerCaminosPorCodigoPostal(int codigoPostal);
-}
